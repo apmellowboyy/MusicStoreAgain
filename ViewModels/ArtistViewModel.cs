cc5a69c13ab5318e4e1c224f546d1a2b836462b9
apmellowboyy <apmellowboyy@gmail.com>
2024-10-22 09:33:37 -0400

    Add project files.

---
+++ ViewModels/ArtistViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicStore.ViewModels
+{
+    public class ArtistViewModel
+    {
+        public List<Album> Albums { get; set; } = new List<Album>();
+        public List<Artist> Artists { get; set; } = new List<Artist>();
+    }
+}
